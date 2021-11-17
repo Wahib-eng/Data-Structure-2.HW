20f7b3f178a0bdb8a783b05d8b97a72336e9b65b
WAHIB HAEL ABDULWARETH MOQBEL <75446834+Wahib-eng@users.noreply.github.com>
2021-11-17 20:48:14 +0300

    Update Question3.c

--- Question3.c
+++ Question3.c
@@ -1,1 +1,26 @@
 
+
+#include <stdio.h>
+
+bool isPrime(int number,int i=2)
+{
+
+	if (number % i == 0)
+		return false;
+	if (i* i > number)
+		return true;
+		
+	return isPrime(number, i+1);
+}
+
+int main (){
+    int number ;
+    printf("Enter a number ");
+    scanf("%d",&number);
+	if (isPrime(number))
+		printf("%d is a prime number " , number );
+	else
+		printf("%d  is not a prime number ",number );
+
+	return 0;
+}
