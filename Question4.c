bed8b6cd7069b6cd55fb45c839f5c226d54af23e
WAHIB HAEL ABDULWARETH MOQBEL <75446834+Wahib-eng@users.noreply.github.com>
2021-11-18 22:07:12 +0300

    Update Question4.c

--- Question4.c
+++ Question4.c
@@ -1,1 +1,29 @@
+#include<stdio.h>
+
+void factors_of_numbers(int number, int i)
+{
+    if(number < 1)
+        return;
+    else if(number % i == 0)
+    {
+      printf("%d\n", i);
+      factors_of_numbers(number / i, i);
+    }
+    else
+    {
+      factors_of_numbers(number, i+1);
+    }
+}
+int main()
+{
+    int number;
+
+    printf("Enter positive number : ");
+    scanf("%d", &number);
+
+    printf("The factors of %d using recursion are : \n", number);
+    factors_of_numbers(number, 2);
+
+    return 0;
+}
 
